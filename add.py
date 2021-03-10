d8721ed51d8a61546d7444bdc77bc868b2ff3dfe
johntygomes <johntygomes30@gmail.com>
2021-03-10 17:50:19 +0530

    main file

---
+++ add.py
@@ -0,0 +1,10 @@
+def abc(x,y):
+    a=x+y
+    print(a)
+abc(5,5)
+
+## git init
+## git remote add origin remoteLink
+## git add fileName / git add . (dot used for all files)
+## git commit -m "comment"
+## git push origin branchName
