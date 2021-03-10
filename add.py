75ea3433bbd8eccdae106a5db6d2ecc87f54bc68
johntygomes <johntygomes30@gmail.com>
2021-03-10 18:02:32 +0530

    main file

--- add.py
+++ add.py
@@ -3,8 +3,14 @@ def abc(x,y):
     print(a)
 abc(5,5)
 
+#  git status
+#  cd to my local repository 
 ## git init
 ## git remote add origin remoteLink
+## git remote -v (just to check ..output should be fetch push)
 ## git add fileName / git add . (dot used for all files)
 ## git commit -m "comment"
 ## git push origin branchName
+
+
+##git remote remove origin
